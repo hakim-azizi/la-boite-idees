79a85925a23e6363e71b963a8b177668c16c1173
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-07-19 19:19:52 +0200

    add schemas database and mockup of a website and complet the style CSS

--- client/src/pages/user/HomeUser.jsx
+++ client/src/pages/user/HomeUser.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 
-// import "../../style/user.css";
+import "../../style/user.css";
 
 function HomeUser() {
   return (
@@ -10,45 +10,47 @@ function HomeUser() {
       </header>
       <main>
         <section className="user">
-          <p>A partir ce panneau vous pourrez</p>
-          <ol>
-            <li>
-              <NavLink to="add-good-idea">ajouter une bonne idées</NavLink>
-            </li>
-            <li>
-              <NavLink to="update-good-idea">
-                Modifier ou supprimer une bonne idées
-              </NavLink>
-            </li>
-          </ol>
-          <ol>
-            <li>
-              <NavLink to="add-bad-idea">ajouter une mauvaise idées</NavLink>
-            </li>
-            <li>
-              <NavLink to="update-bad-idea">
-                Modifier ou supprimer une mauvaise idées
-              </NavLink>
-            </li>
-          </ol>
-          <ol>
-            <li>
-              <NavLink to="add-user">Ajouter un membre</NavLink>
-            </li>
-            <li>
-              <NavLink to="update-user">
-                Modifier ou supprimer un membre
-              </NavLink>
-            </li>
-          </ol>
-          <ol>
-            <li>
-              <NavLink to="update-status-user">
-                Modifier le status d'un membre pour le faire passser
-                administrateur
-              </NavLink>
-            </li>
-          </ol>
+          <article>
+            <p>A partir ce panneau vous pourrez</p>
+            <ol>
+              <li>
+                <NavLink to="add-good-idea">ajouter une bonne idées</NavLink>
+              </li>
+              <li>
+                <NavLink to="update-good-idea">
+                  Modifier ou supprimer une bonne idées
+                </NavLink>
+              </li>
+            </ol>
+            <ol>
+              <li>
+                <NavLink to="add-bad-idea">ajouter une mauvaise idées</NavLink>
+              </li>
+              <li>
+                <NavLink to="update-bad-idea">
+                  Modifier ou supprimer une mauvaise idées
+                </NavLink>
+              </li>
+            </ol>
+            <ol>
+              <li>
+                <NavLink to="add-user">Ajouter un membre</NavLink>
+              </li>
+              <li>
+                <NavLink to="update-user">
+                  Modifier ou supprimer un membre
+                </NavLink>
+              </li>
+            </ol>
+            <ol>
+              <li>
+                <NavLink to="update-status-user">
+                  Modifier le status d'un membre pour le faire passser
+                  administrateur
+                </NavLink>
+              </li>
+            </ol>
+          </article>
         </section>
       </main>
     </>
